e44cb62e6b81df973dacbf58e4b978d25c72c82c
Degeneve Hugo <hugo.degeneve@student-cs.fr>
2023-08-10 13:53:53 +0200

    seed is set to 42

--- network/topology.py
+++ network/topology.py
@@ -1,9 +1,21 @@
+import torch
 import torch.nn as nn
+
+# Set a seed for the random number generator
+seed_value = 42
+
+def set_seed():
+    torch.manual_seed(seed_value)
+    # If using CUDA (GPU), also set the seed for GPU
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed_value)
 
 
 class DQN_1(nn.Module):
     
     def __init__(self, n_inputs):
+        set_seed()
+
         nn.Module.__init__(self) # Tell torch that this class is a neural network
 
         #### Write your code here for task 12
@@ -26,6 +38,8 @@ class DQN_1(nn.Module):
 class DQN_2(nn.Module):
     
     def __init__(self, n_inputs):
+        set_seed()
+
         nn.Module.__init__(self) # Tell torch that this class is a neural network
 
         #### Write your code here for task 12
