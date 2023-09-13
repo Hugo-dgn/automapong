79fd5e96b56c35b1c7ec8f6b4b6e9f0ebbbe589e
Hugo Degeneve <hugo.degeneve@gmail.com>
2023-09-13 20:20:17 +0200

    new dqn

--- network/topology.py
+++ network/topology.py
@@ -20,3 +20,25 @@ class DQN_1(nn.Module):
         #### Write your code here for task 12
         return self.network(x)
         ####
+
+class DQN_2(nn.Module):
+    
+    def __init__(self, n_inputs):
+        nn.Module.__init__(self) # Tell torch that this class is a neural network
+
+        #### Write your code here for task 12
+        self.network = nn.Sequential(
+            nn.Linear(n_inputs, 64), 
+            nn.ReLU(),
+            nn.Linear(64, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 64)
+            nn.ReLU(),
+            nn.Linear(64, 3)
+        )
+        ####
+    
+    def forward(self, x):
+        #### Write your code here for task 12
+        return self.network(x)
+        ####
